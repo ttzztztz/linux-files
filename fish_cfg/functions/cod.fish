ba74412d61d60bbcb3b58a85c47280aa12dda0d8
rabbit <ttzztztz@outlook.com>
2024-03-09 09:18:02 +0800

    fix bugs

--- fish_cfg/functions/cod.fish
+++
@@ -1,3 +0,0 @@
-function cod --wraps=code --description 'alias cod=code'
-  code --enable-features=UseOzonePlatform,WaylandWindowDecorations --ozone-platform=wayland --enable-features=Vulkan $argv
-end
---
+++ fish_cfg/functions/code.fish
@@ -0,0 +1,4 @@
+function code --description 'code parameters for wayland'
+ command code $argv --enable-features=UseOzonePlatform,WaylandWindowDecorations,WebRTCPipeWireCapturer --ozone-platform=wayland --enable-features=Vulkan --enable-wayland-ime
+        
+end
