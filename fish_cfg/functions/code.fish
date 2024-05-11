e56d32d00661076a2bbd774c5b4b1052a80055e5
Rabbit <ttzztztz@outlook.com>
2024-05-11 15:57:05 +0800

    Update code.fish

--- fish_cfg/functions/code.fish
+++ fish_cfg/functions/code.fish
@@ -1,4 +1,4 @@
 function code --description 'code parameters for wayland'
- command code $argv --enable-features=UseOzonePlatform,WaylandWindowDecorations,WebRTCPipeWireCapturer --ozone-platform=wayland --enable-features=Vulkan --enable-wayland-ime
+ command code $argv --enable-features=UseOzonePlatform,WaylandWindowDecorations,WebRTCPipeWireCapturer --ozone-platform=wayland --enable-wayland-ime
         
 end
