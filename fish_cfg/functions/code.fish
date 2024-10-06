6228a62edf61cf4d4996692fe5e3d42cc533ac39
rabbit <ttzztztz@outlook.com>
2024-10-06 18:52:58 +0800

    remove code

--- fish_cfg/functions/code.fish
+++
@@ -1,4 +0,0 @@
-function code --description 'code parameters for wayland'
- command code $argv --enable-features=UseOzonePlatform,WaylandWindowDecorations,WebRTCPipeWireCapturer --ozone-platform=wayland --enable-wayland-ime
-        
-end
